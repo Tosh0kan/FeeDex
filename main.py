52f7e861bc63565499cd7a0755196386e08d6370
Tosh0kan <toteshokan@gmail.com>
2023-01-17 20:13:44 -0300

    Reworked save_settings func, and added more menu options

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import json
 import httpx
 
 
-class Globals:
-    global base_url
+class Arrays:
     base_url = 'https://api.mangadex.org/manga/'
+    manga_list = []
+    settings_dict = {}
 
 
 class Guncs:
@@ -22,9 +23,11 @@ class Guncs:
         global first_time_use
         try:
             with open(Guncs.resource_path('manga_notification_settings.json'), 'r', encoding='utf-8') as f:
-                global settings_dict
-                settings_dict = f.read()
+                Arrays.settings_dict = json.loads(f.read())
             first_time_use = False
+
+            for key in Arrays.settings_dict.keys():
+                Arrays.manga_list.append(key)
 
         except FileNotFoundError:
             first_time_use = True
@@ -43,21 +46,42 @@ class Guncs:
                     return
 
                 else:
-                    print("Invalid URL. Please try again.\n\n")
+                    print("Invalid URL. Please try again.\n")
 
             else:
-                pass
+                fst_menu_choice = input("\nChoose one of the options below:\n1. Add/Manage subscriptions\n\n")
+
+                if '1' in fst_menu_choice:
+                    print("\nCurrently subscribed mangas are:\n")
+                    while True:
+                        for manga in Arrays.manga_list:
+                            print('\n', manga, sep='')
+                        scnd_menu_choice = input(("\nChoose one of the options below:\n1. Add manga to subscriptions\n2. Remove manga from subscriptions\n3. Previous menu\n\n"))
+
+                        if '1' in scnd_menu_choice:
+                            manga_url = input("\nPlease, paste the URL of the manga's main page\n")
+                            manga_title, manga_id, most_recent_date = Guncs.get_inital_manga_state(manga_url)
+                            Guncs.save_settings(manga_title, manga_id, most_recent_date)
+
+                        elif '2' in scnd_menu_choice:
+                            pass
+
+                        elif '3' in scnd_menu_choice:
+                            break
+
+                else:
+                    print("\nInvalid input. Please try again.\n")
 
     @staticmethod
     def get_inital_manga_state(manga_url: str):
         manga_id = re.split(r'.+title/([^/]+).+', manga_url)
         manga_id = ''.join(manga_id)
         r_title = httpx.get(
-            f'{base_url}{manga_id}',
+            f'{Arrays.base_url}{manga_id}',
             follow_redirects=True
         )
         r_date = httpx.get(
-            f'{base_url}{manga_id}/feed',
+            f'{Arrays.base_url}{manga_id}/feed',
             follow_redirects=True,
             params={
                 "translatedLanguage[]": "en",
@@ -74,15 +98,21 @@ class Guncs:
     @staticmethod
     def save_settings(manga_title: str, manga_id: str, most_recent_date: str):
         if first_time_use:
-            settings_dict = {}
-            settings_dict.setdefault(manga_title, []).append(manga_id), settings_dict.setdefault(manga_title, []).append(most_recent_date)
+            Arrays.settings_dict.setdefault(manga_title, []).append(manga_id)
+            Arrays.settings_dict.setdefault(manga_title, []).append(most_recent_date)
 
             with open(Guncs.resource_path('manga_notification_settings.json'), 'w', encoding='utf-8') as f:
-                f.write(json.dumps(settings_dict))
+                f.write(json.dumps(Arrays.settings_dict, indent=4))
 
         else:
-            with open(Guncs.resource_path('manga_notification_settings.json'), 'a', encoding='utf-8') as f:
-                f.write(json.dumps(settings_dict))
+            new_sub = {}
+            new_sub.setdefault(manga_title, []).append(manga_id)
+            new_sub.setdefault(manga_title, []).append(most_recent_date)
+            Arrays.settings_dict.update(new_sub)
+            Arrays.manga_list.append(manga_title)
+
+            with open(Guncs.resource_path('manga_notification_settings.json'), 'w', encoding='utf-8') as f:
+                f.write(json.dumps(Arrays.settings_dict, indent=4))
 
     @staticmethod
     def main():
